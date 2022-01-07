aa992b4a85108c95d5ec0b473d79cb970cd502ad
jordaneaster <jordane@am-collab.com>
2022-01-07 14:23:45 -0500

    take out smoke shop

--- pages/Events.vue
+++ pages/Events.vue
@@ -2,12 +2,14 @@
   <div class="event_container">
     <Nav />
     <div class="event_sm">
-      <img class="fbs_e" src="../static/icn-facebook.svg" />
-      <img class="igs_e" src="../static/icn-instagram.svg" />
-      <img class="tws_e" src="../static/icn-negative.svg" />
+      <img class="fbs_e" src="../static/icn-facebook.svg">
+      <img class="igs_e" src="../static/icn-instagram.svg">
+      <img class="tws_e" src="../static/icn-negative.svg">
     </div>
     <div class="event_hero" />
-    <h1 class="event_heading">Cater Your Next Event Here!</h1>
+    <h1 class="event_heading">
+      Cater Your Next Event Here!
+    </h1>
     <div class="event_para">
       <div class="abc">
         <p class="paras">
@@ -18,32 +20,44 @@
       </div>
       <div class="abc">
         <ul class="event_lists">
-          <li class="event_list_items">2 Private Rooms</li>
-          <li class="event_list_items">Hookah Specials</li>
-          <li class="event_list_items">Drink Specials</li>
-          <li class="event_list_items">6 flat screens</li>
-          <li class="event_list_items">Music</li>
+          <li class="event_list_items">
+            2 Private Rooms
+          </li>
+          <li class="event_list_items">
+            Hookah Specials
+          </li>
+          <li class="event_list_items">
+            Drink Specials
+          </li>
+          <li class="event_list_items">
+            6 flat screens
+          </li>
+          <li class="event_list_items">
+            Music
+          </li>
         </ul>
       </div>
     </div>
     <div class="abcc">
       <nuxt-link to="/reservations">
-        <button class="event_schedules">Schedule Now!</button>
+        <button class="event_schedules">
+          Schedule Now!
+        </button>
       </nuxt-link>
     </div>
     <div class="event_content">
       <div>
-        <img class="event_img" src="../static/reservation4.jpg" />
+        <img class="event_img" src="../static/reservation4.jpg">
       </div>
       <div>
-        <img class="event_img" src="../static/reservation3.jpg" />
+        <img class="event_img" src="../static/reservation3.jpg">
       </div>
     </div>
 
     <div class="event_sm">
-      <img class="fbs_e" src="../static/icn-facebook.svg" />
-      <img class="igs_e" src="../static/icn-instagram.svg" />
-      <img class="tws_e" src="../static/icn-negative.svg" />
+      <img class="fbs_e" src="../static/icn-facebook.svg">
+      <img class="igs_e" src="../static/icn-instagram.svg">
+      <img class="tws_e" src="../static/icn-negative.svg">
     </div>
   </div>
 </template>
